034aa3af24f54af2b0df42e1b02e6fd41a63abc7
Julio Neto <nettojulio@hotmail.com>
2022-02-09 22:26:01 -0300

    1003 OK

---
+++ BeeCrowd/BeeCrowd1003.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BeeCrowd1003 {
+    public static void main(String args[]){
+        Scanner teclado = new Scanner(System.in);
+
+        int A,B,SOMA;
+
+        A = teclado.nextInt();
+        B = teclado.nextInt();
+        
+        SOMA = A + B;
+
+        System.out.println("SOMA = " + SOMA);
+        
+        teclado.close();
+    }
+}
