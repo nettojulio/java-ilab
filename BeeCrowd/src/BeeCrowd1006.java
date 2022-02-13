331559e596b962ce30c6b0dc8d73f4c58524ddeb
Julio Neto <nettojulio@hotmail.com>
2022-02-13 16:24:03 -0300

    1006 OK

---
+++ BeeCrowd/src/BeeCrowd1006.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BeeCrowd1006 {
+	public static void main(String args[]) {
+		Scanner teclado = new Scanner(System.in);
+
+		double a, b, c, media;
+
+		a = teclado.nextDouble();
+		b = teclado.nextDouble();
+		c = teclado.nextDouble();
+
+		media = (a * 2 + b * 3 + c * 5) / 10;
+
+		System.out.printf("MEDIA = %.1f\n", media);
+
+		teclado.close();
+	}
+}
